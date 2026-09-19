b2d53177fb8070767b83ef71fa79bf062030828e
vladimirlihacky <vladimirlihacky@gmail.com>
2026-09-19 14:06:31 +0300

    update graphql schema

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import { Neo4jGraphQL } from "@neo4j/graphql";
+import neo4j from "neo4j-driver"
+import { ApolloServer } from "apollo-server"
+
+const typeDefs = await Bun.file("./schema.gql").text()
+const driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "password"))
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver })
+
+const schema = await neoSchema.getSchema()
+
+const server = new ApolloServer({
+    schema, 
+    context: ({ req }) => ({ req }),
+})
+
+await server.listen(4000)
+console.log("Online")
