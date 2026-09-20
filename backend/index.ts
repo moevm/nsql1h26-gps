fc577d1dcd59a9658a85fd0f15ea1f7d0e4860c5
vladimirlihacky <vladimirlihacky@gmail.com>
2026-09-20 20:21:08 +0300

    decided to use cypher instead of graphql

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,2 @@
-import { schema } from "./src/graphql";
 import Elysia from "elysia";
-import { openapi } from "@elysia/openapi"
-import { apollo } from "@elysia/apollo"
-
-new Elysia()
-    .use(openapi())
-    .use(apollo({ schema }))
-    .listen(3000)
+import { openapi } from "@elysia/openapi";
