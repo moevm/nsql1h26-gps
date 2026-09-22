00e9efb1216fd84d51eef147d0cd6d427b2c6d61
vladimirlihacky <vladimirlihacky@gmail.com>
2026-09-22 11:28:08 +0300

    done

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,22 @@
+import { mkdir } from "node:fs/promises";
 import config from "./config";
 import { buildApp } from "./src/app";
 import { ensureConstraints } from "./src/bootstrap/constraints";
 import { seedIfEmpty } from "./src/bootstrap/seed";
 import { withRetry } from "./src/common/db";
 
+process.on("uncaughtException", (e) => {
+  console.error("[uncaughtException]", e);
+});
+process.on("unhandledRejection", (e) => {
+  console.error("[unhandledRejection]", e);
+});
+
 const app = buildApp();
 app.listen(config.PORT);
 console.log(`[server] listening on :${config.PORT}`);
+
+void mkdir(config.UPLOADS_DIR, { recursive: true }).catch(() => {});
 
 void withRetry(async () => {
   await ensureConstraints();
